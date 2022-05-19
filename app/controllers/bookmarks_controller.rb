e432cca5d75631b2a3fdca040cca7258f9de4807
shane gaignard <shane.c.gaignard@gmail.com>
2022-05-19 16:38:23 +0200

    finished app for today

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -20,6 +20,11 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+
+    redirect_to list_path(@list)
   end
 
   private
